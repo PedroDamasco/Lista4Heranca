4620e7365e27738bae8dfc1a240df2604f04292d
PedroDamasco <103971452+PedroDamasco@users.noreply.github.com>
2023-05-19 16:46:13 -0300

    Add files via upload

---
+++ Ex01/FuncionarioMensalista.java
@@ -0,0 +1,32 @@
+package Ex01;
+
+public class FuncionarioMensalista extends Funcionario {
+
+    private double salario;
+
+    public FuncionarioMensalista() {
+    }
+
+    public FuncionarioMensalista(int numeroCracha, String nome, String setor, String funcao, double salario) {
+        setNome(nome);
+        setFuncao(funcao);
+        setNumeroCracha(numeroCracha);
+        setSetor(setor);
+        this.salario = salario;
+    }
+
+    public String imprimir() {
+        return "Numero do Cracha: " + getNumeroCracha() + " Nome: " + getNome() + " Setor: " + getSetor() + " Função: " + getFuncao() + " Salario: " + salario;
+    }
+    
+
+    /* Get e Set's */
+    
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
---
+++ Ex01/MainEx01.java
@@ -0,0 +1,91 @@
+package Ex01;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainEx01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Funcionario> funcionarioList = new ArrayList<>();
+
+        int opcao = 0;
+        while (opcao != 3) {
+            System.out.println("\nMenu:");
+            System.out.println("1 - Criar Funcionário");
+            System.out.println("2 - Exibir Funcionários");
+            System.out.println("3 - Sair");
+
+            System.out.print("\nEscolha uma opção: ");
+            opcao = sc.nextInt();
+            sc.nextLine(); // Limpar o buffer do scanner
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Número do Cartão: ");
+                    int nmCartao = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Nome: ");
+                    String nome = sc.nextLine();
+
+                    System.out.println("Setor: ");
+                    String setor = sc.nextLine();
+
+                    System.out.println("Função: ");
+                    String funcao = sc.nextLine();
+
+                    System.out.println("1 - Horista\n2 - Mensalista");
+                    int opc2 = sc.nextInt();
+
+                    if (opc2 == 1) {
+                        System.out.println("Quantidade de Horas: ");
+                        int qtdeHoras = sc.nextInt();
+
+                        System.out.println("Valor da Hora: ");
+                        double valorHora = sc.nextDouble();
+
+                        FuncionarioHorista f1 = new FuncionarioHorista(nmCartao, nome, setor, funcao, qtdeHoras, valorHora);
+                        funcionarioList.add(f1);
+                    } else if (opc2 == 2) {
+                        System.out.println("Salário: ");
+                        double salario = sc.nextDouble();
+
+                        FuncionarioMensalista f2 = new FuncionarioMensalista(nmCartao, nome, setor, funcao, salario);
+                        funcionarioList.add(f2);
+                    }
+                    break;
+                case 2:
+                    System.out.println("1 - Horista\n2 - Mensalista");
+                    int opc3 = sc.nextInt();
+
+                    if (opc3 == 1) {
+                        System.out.println("\nHoristas:");
+                        for (Funcionario funcionario : funcionarioList) {
+                            if (funcionario instanceof FuncionarioHorista) {
+                                FuncionarioHorista horista = (FuncionarioHorista) funcionario;
+                                System.out.println(horista.imprimir());
+                            }
+                        }
+                    } else if (opc3 == 2) {
+                        System.out.println("\nMensalistas:");
+                        for (Funcionario funcionario : funcionarioList) {
+                            if (funcionario instanceof FuncionarioMensalista) {
+                                FuncionarioMensalista mensalista = (FuncionarioMensalista) funcionario;
+                                System.out.println(mensalista.imprimir());
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
+            }
+        }
+
+        sc.close();
+    }
+}
